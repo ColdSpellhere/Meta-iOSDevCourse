7fba2737a6d70fc3efed8a3a3ffb13b8bbd54a8f
ColdSpell <coldspell060319@gmail.com>
2025-10-31 21:23:01 +0800

    Add files via upload

---
+++ Course1-Introduction_to_iOSMobileApplicationDevelopment/vido_player_test/vido_player_testTests/vido_player_testTests.swift
@@ -0,0 +1,17 @@
+//
+//  vido_player_testTests.swift
+//  vido_player_testTests
+//
+//  Created by ΧiaoLuhan on 2025/10/31.
+//
+
+import Testing
+@testable import vido_player_test
+
+struct vido_player_testTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
---
+++ Course1-Introduction_to_iOSMobileApplicationDevelopment/vido_player_test/vido_player_testUITests/vido_player_testUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  vido_player_testUITestsLaunchTests.swift
+//  vido_player_testUITests
+//
+//  Created by ΧiaoLuhan on 2025/10/31.
+//
+
+import XCTest
+
+final class vido_player_testUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
